092a0307c1e0d595f0554f362a602f6ae479b015
gus15378 <92408350+gus15378@users.noreply.github.com>
2021-11-06 23:38:55 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join("Resources","budget_data.csv")
+csvpath = os.path.join("budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
