ba9fea1592752e8b54cb65072d0a8f83cdb2f641
gus15378 <92408350+gus15378@users.noreply.github.com>
2021-11-06 23:36:53 -0500

    Create main.py

---
+++ PyBank/main.py
@@ -0,0 +1,31 @@
+import os
+import csv
+
+csvpath = os.path.join("Resources","budget_data.csv")
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    
+    month = []
+    profit = []
+    change = []
+    
+    for row in csvreader:
+        month.append(row[0])
+        profit.append(int(row[1]))
+    for i in range(len(profit)-1):
+        change.append(profit[i])
+        
+increase = max(change)
+decrease = min(change)
+
+month_increase = change.index(max(change))+1
+month_decrease = change.index(min(change))+1
+
+print("Financial Analysis")
+print(f"Total Months:{len(month)}")
+print(f"Total: ${sum(profit)}")
+print(f"Average Change: {round(sum(change)/len(change),2)}")
+print(f"Greatest Increase in Profits: {month[month_increase]} (${(str(increase))})")
+print(f"Greatest Increase in Profits: {month[month_decrease]} (${(str(decrease))})")
